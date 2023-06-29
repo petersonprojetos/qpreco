46f432847963163836c69a3f38ed754a268244a9
petersonprojetos <petersonrocha@gmail.com>
2023-06-29 12:30:03 -0300

    Ajustes js
    Ajustes realizados nos campos

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
+function addIntegracao(){
+    
+    let integracao = document.getElementById('selectIntegracao')
+    let ativar = document.getElementById('ativiarDivIntegracao')
+
+    integracao.addEventListener('change', function(){
+        
+        if (integracao.value == 'sim' || integracao.value == 'talvez') {
+           ativar.hidden=false;
+        } else {ativar.hidden=true;}
+        
+    })
+}
+
 function appMovel(){
     
     let appMovel = document.getElementById('selectAppMovel')
+    let ativar = document.getElementById('ativiarDivMovel')
 
     appMovel.addEventListener('change', function(){
         
         if (appMovel.value == 'sim') {
             
-            document.getElementById('addDiv').innerHTML = '<div class="input-group" id="addAppMovel"><div class="input-group-prepend"><span class="input-group-text" id="quaisApps">Quais Apps para dispositivos móveis?</span></div><input type="text" class="form-control" placeholder="Liste os nomes dos apps ou para que eles servem..." name="quaisApps" aria-label="Apps para dispositivos móveis" aria-describedby="basic-addon1" required/></div>'
-        } else {document.getElementById('addDiv').innerHTML = ''}
-        
-    })
-}
-
-function addIntegracao(){
-    
-    let integracao = document.getElementById('selectIntegracao')
-
-    integracao.addEventListener('change', function(){
-        
-        if (integracao.value != 'não') {
-            
-            document.getElementById('addDiv').innerHTML = '<div class="input-group" id="addIntegracao"><div class="input-group-prepend"><span class="input-group-text" id="integrcaoApi">Tais sistemas possuem documentações de APIs?</span></div><input type="text" class="form-control" placeholder="Informe se possuem documentação das APIs para integração..." name="integracaoApi" aria-label="Integração via Apis" aria-describedby="basic-addon1" /></div>'
-        } else {document.getElementById('addDiv').innerHTML = ''}
+            ativar.hidden=false;
+        } else {ativar.hidden=true;}
         
     })
 }
